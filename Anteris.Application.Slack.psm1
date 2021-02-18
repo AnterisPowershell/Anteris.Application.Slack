a4577b0e7f6487ac8cb0ab3caa17a4292744192c
Aidan Casey <aidan@caseyhouse.net>
2021-02-18 15:03:06 -0500

    Adds basic support for Mac OS and updates version.

--- Anteris.Application.Slack.psm1
+++ Anteris.Application.Slack.psm1
@@ -7,10 +7,21 @@
    folder and checking their appdata.
 #>
 function Test-SlackInstalled {
-    $installs = Get-ChildItem -Path "$($env:SystemPath)\Users\*\AppData\Local\slack";
+    if ($IsWindows) {
+        $installs = Get-ChildItem -Path "$($env:SystemPath)\Users\*\AppData\Local\slack";
 
-    if ($installs.Count -gt 0) {
-        return $true;
+        if ($installs.Count -gt 0) {
+            return $true;
+        }
+    }
+
+    if ($IsMacOS) {
+        $globalInstalls = Get-ChildItem -Path "/Applications/Slack.app";
+        $userInstalls = Get-ChildItem -Path "/Users/*/Applications/Slack.app";
+
+        if ($globalInstalls.Count -gt 0 -or $userInstalls -gt 0) {
+            return $true;
+        }
     }
 
     return $false;
@@ -24,12 +35,29 @@ function Test-SlackInstalled {
    Iterates through each user's AppData folders and removes Slack from their profile.
 #>
 function Uninstall-Slack {
-    $installs = Get-ChildItem -Path "${$env:SystemPath}\Users\*\AppData\Local\slack";
+    if ($IsWindows) {
+        $installs = Get-ChildItem -Path "${$env:SystemPath}\Users\*\AppData\Local\slack";
 
-    foreach ($install in $installs) {
-        $command = "$($install.FullName)\Update.exe";
+        foreach ($install in $installs) {
+            $command = "$($install.FullName)\Update.exe";
 
-        # Run the Slack uninstaller for the indivdual user
-        & $command --uninstall -s
+            # Run the Slack uninstaller for the indivdual user
+            & $command --uninstall -s
+        }
+
+        return;
     }
+
+    if ($IsMacOS) {
+        $installs = @(Get-ChildItem -Path "/Applications/Slack.app");
+        $installs += @(Get-ChildItem -Path "/Users/*/Applications/Slack.app");
+
+        foreach ($install in $installs) {
+            Remove-Item $install -Recurse -Force;
+        }
+
+        return;
+    }
+
+    throw("Unfortunately, your OS is not supported by this function.");
 }
